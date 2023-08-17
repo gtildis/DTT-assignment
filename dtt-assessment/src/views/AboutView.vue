fa4f6a213a19509bb010f4ca13e325bdcf1f5bfb
gtildis <gretil_15@hotmail.com>
2023-08-17 22:20:59 +0200

    Bonus no.5: About Page

--- dtt-assessment/src/views/AboutView.vue
+++ dtt-assessment/src/views/AboutView.vue
@@ -7,18 +7,24 @@
 				reiciendis totam temporibus harum minus vel eum accusantium eligendi
 				voluptas ex possimus minima pariatur vero obcaecati, odit, fugit quam
 				deserunt non!Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci
+				reiciendis totam temporibus harum minus vel eum accusantium eligendi
+				voluptas ex possimus minima pariatur vero obcaecati, odit, fugit quam
+				deserunt non!Lorem ipsum dolor sit amet consectetur, adipisicing elit.
 			</p>
 
 			<p>
 				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci
 				reiciendis totam temporibus harum minus vel eum accusantium
 			</p>
-			<h1>Design adn Development</h1>
+			<h1>Design and Development</h1>
 			<div class="logo-container">
 				<img src="../assets/img_logo_dtt@3x.png" alt="DTT logo" />
 				<div class="link-container">
 					<p>By DTT</p>
-					<a href="https://www.d-tt.nl/">https://www.d-tt.nl/</a>
+					<a href="https://www.d-tt.nl/" target="_blank"
+						>https://www.d-tt.nl/</a
+					>
 				</div>
 			</div>
 		</div>
@@ -44,10 +50,24 @@ h1 {
 
 .about-container {
 	width: 70%;
+	margin: 2rem 0;
+}
+.about-container p {
+	margin: 2.5rem 0;
 }
 
 .logo-container {
 	display: flex;
+	align-items: center;
+}
+
+.logo-container img {
+	height: 70px;
+	margin-right: 2rem;
+}
+
+.link-container p {
+	margin: 0.5rem 0;
 }
 
 a {
